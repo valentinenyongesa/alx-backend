acae244748754d9dc3126aa60b3a4afe0fa34693
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-03 01:40:26 +0000

    Basic dictionary that is a caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class inherits from BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+
+
+if __name__ == "__main__":
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    print(my_cache.get("D"))
+    my_cache.print_cache()
+    my_cache.put("D", "School")
+    my_cache.put("E", "Battery")
+    my_cache.put("A", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
---
+++ 0x01-caching/README.md
@@ -0,0 +1,2 @@
+0x01. Caching
+This project covers a concept called Caching in the Backend
