1c9715957c197fac4250d94c17f41fa4f53c5fb5
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-05-06 23:22:25 +0000

    Using hgetall, display the object stored in Redis

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,39 @@
+// Import the required library
+import redis from 'redis';
+
+// Create a new Redis client
+const client = redis.createClient();
+
+// Handle the connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+// Function to create a hash in Redis
+function createHash() {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+}
+
+// Function to display the hash stored in Redis
+function displayHash() {
+  client.hgetall('HolbertonSchools', (err, reply) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(reply);
+  });
+}
+
+// Call the functions
+createHash();
+displayHash();
